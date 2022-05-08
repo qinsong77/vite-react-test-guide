089c7366eeba4b90c5ac6acdbfe6cadd1e8b946c
notend <xyqinsong@163.com>
2022-05-08 19:55:03 +0800

    fix: lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,9 @@ module.exports = {
     es2021: true
   },
   extends: [
-    'plugin:react/recommended',
+    'plugin:react/recommended', //
     'standard',
-    'plugin:prettier/recommended',
+    'plugin:prettier/recommended'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -17,11 +17,16 @@ module.exports = {
     sourceType: 'module'
   },
   plugins: [
-    'react',
+    'react', //
     '@typescript-eslint',
     'prettier'
   ],
   rules: {
-    "prettier/prettier": "error"
+    'prettier/prettier': 'error',
+    // suppress errors for missing 'import React' in files
+    'react/react-in-jsx-scope': 'off',
+    // 'react/jsx-first-prop-new-line': [1, 'multiline'],
+    // https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/jsx-max-props-per-line.md
+    'react/jsx-max-props-per-line': [1, { when: 'multiline' }]
   }
 }
